9b5f337d0f76c8bfb9c1f7bff6b08c7ea4de2486
Efe Ariaroo <efeariaroo1@gmail.com>
2018-09-23 23:08:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var express = require("express");
+
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+
+
+app.post('/login', function (req, res) {
+  res.status(400).json({ "status": "Inputs for login" });
+});
+
+app.post('/signup', function (req, res) {
+  res.status(400).json({ "status": "Inputs for signup" });
+});
+
+app.post('/newNote', function (req, res) {
+  res.status(400).json({ "status": "Inputs for newNote" });
+});
+
+app.post('/editNote', function (req, res) {
+  res.status(400).json({ "status": "Inputs for editNote" });
+});
+
+var server = app.listen(3000);
